8c39737f22182e2f8e58d212cca98f4e7806c061
Mickey Hu <18319022360@163.com>
2017-03-15 17:10:32 +0800

    初始化项目文件

---
+++ implementation/src/Mickey.ThinkingInDoNet/Mickey.ThinkingInDoNet/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mickey.ThinkingInDoNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"F:\git\mickey.thinkingInDoNet\implementation\src\Mickey.ThinkingInDoNet\Mickey.ThinkingInDoNet\test.txt";
+            var test = File.ReadAllText(path);
+
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                sw.Write(test.Replace("1", "55"));
+                sw.Flush();
+            }
+            Console.Write(File.ReadAllText(path));
+
+            Console.ReadLine();
+        }
+    }
+}

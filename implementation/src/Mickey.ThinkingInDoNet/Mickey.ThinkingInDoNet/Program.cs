58ece7d3783738635b1824cfbdc79e4b5d3bd416
mickeyhu <mickeyhu@tencent.com>
2017-04-17 18:55:14 +0800

    测试一下

--- implementation/src/Mickey.ThinkingInDoNet/Mickey.ThinkingInDoNet/Program.cs
+++ implementation/src/Mickey.ThinkingInDoNet/Mickey.ThinkingInDoNet/Program.cs
@@ -18,6 +18,8 @@ namespace Mickey.ThinkingInDoNet
             {
                 File.AppendAllText(path, "\n222222");
             }
+
+
             Console.Write(File.ReadAllText(path));
             Console.ReadLine();
         }

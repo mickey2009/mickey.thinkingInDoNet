0c52d2f6d20bff7beaebd834ec289c7e53a8027a
mickeyhu <mickeyhu@tencent.com>
2017-06-05 11:10:37 +0800

    添加SignalR的测试程序

---
+++ implementation/src/Mickey.ThinkingInDoNet/Mickey.SignalRChat/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Mickey.SignalRChat.Startup))]
+
+namespace Mickey.SignalRChat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
